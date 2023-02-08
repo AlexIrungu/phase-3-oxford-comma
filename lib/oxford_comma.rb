c3bac2d54c1dc9e54ed65f465b468c903716462d
AlexIrungu <aleaxmuiruri@gmail.com>
2023-02-08 21:28:04 +0300

    Completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array[0]
+    elsif array.length == 2
+        arr = ["kiwi", "dorian"]
+        array.join( " and " )
+    else
+        arr = ["kiwi", "dorian", "starfruit"]
+        last_element = array.pop
+        array.join(", ") + ", and " + last_element
+    end
 end
